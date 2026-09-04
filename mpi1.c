01e429801de6630381aabe2fe376c23da9c5f71f
Ubuntu <ubuntu@ip-172-31-22-8.ec2.internal>
2026-09-04 20:43:30 +0000

    adiciona operacoes e soma dos resultados paralelos

--- mpi1.c
+++ mpi1.c
@@ -5,6 +5,10 @@
 int main(void) {
    int        comm_sz;               /* Número de processos    */
    int        my_rank;               /* Rank   do processo     */
+   int        a = 2;
+   int        b = 3;
+   
+  
 
    /* Inicia MPI */
    MPI_Init(NULL, NULL); 
@@ -16,7 +20,42 @@ int main(void) {
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
 
    printf("Número de processos: %d Rank do processos corrente: %d \n", comm_sz, my_rank);
+   if(my_rank != 0){
+      int resultado = 0;
+      if (my_rank == 1){
+         int resultado = a+b;
+         printf("a + b = %d\n",resultado);
+         MPI_Send(&resultado, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
+      }
+         if (my_rank == 2){
+         int resultado = b-a;
+         printf("b - a = %d\n",resultado);
+         MPI_Send(&resultado, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
+      }
+         if (my_rank == 3){
+         int resultado = a*b;
+         printf("a * b = %d\n",resultado);
+         MPI_Send(&resultado, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
+      }
+         if (my_rank == 4){
+         int resultado = a - b;
+         printf("a - b = %d\n",resultado);
+         MPI_Send(&resultado, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
+      }
+   }else{
+      int resultado = 0;
+      int final = 0;
+      for (int q = 1; q < comm_sz; q++) {
+        
+         /* Recepção da mensagem do processo q */
+         MPI_Recv(&resultado ,1, MPI_INT, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+         final+=resultado;
 
+      } 
+      printf("Soma de todos os resultados = %d ",final);
+   }
+   
+   
    /* Encerra MPI */
    MPI_Finalize(); 
 
